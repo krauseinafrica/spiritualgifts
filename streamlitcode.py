ed87fc420b54077a10c3435ded60af2d03ebb748
krauseinafrica <krauseinafrica@icloud.com>
2025-01-26 14:18:00 -0500

    Update streamlitcode.py

--- streamlitcode.py
+++ streamlitcode.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.title("Discover Your Spiritual Gifts")
+st.subheader("Uncover how God has uniquely gifted you to serve!")
+st.write("""
+Take this fun and interactive assessment to find out your spiritual gifts and how you can use them to glorify God and serve others. 
+""")
+if st.button("Start the Game"):
+    st.session_state.start_game = True
